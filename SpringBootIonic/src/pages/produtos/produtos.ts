90962215155f8526911975da6221ee4bc0241c67
Jefferson dos santos <Jefferson1709@hotmail.com>
2021-10-21 20:44:26 -0300

    Criando pagina de carrinho de compras e funções e botao flutuante

--- SpringBootIonic/src/pages/produtos/produtos.ts
+++ SpringBootIonic/src/pages/produtos/produtos.ts
@@ -11,42 +11,42 @@ import { ProdutoService } from '../../services/domain/produto.service';
  * Ionic pages and navigation.
  */
  @IonicPage()
- @Component({
-   selector: 'page-produtos',
-   templateUrl: 'produtos.html',
- })
- export class ProdutosPage {
+@Component({
+  selector: 'page-produtos',
+  templateUrl: 'produtos.html',
+})
+export class ProdutosPage {
 
-   items : ProdutoDTO[];
+  items : ProdutoDTO[];
 
-   constructor( public navCtrl: NavController, public navParams: NavParams,
+  constructor( public navCtrl: NavController, public navParams: NavParams,
     public produtoService: ProdutoService) {
 
-   }
+  }
 
-   ionViewDidLoad() {
-     let categoria_id = this.navParams.get('categoria_id');
-     this.produtoService.findByCategoria(categoria_id)
-       .subscribe(response => {
-         this.items = response['content'];
-         this.loadImageUrls();
+  ionViewDidLoad() {
+    let categoria_id = this.navParams.get('categoria_id');
+    this.produtoService.findByCategoria(categoria_id)
+      .subscribe(response => {
+        this.items = response['content'];
+        this.loadImageUrls();
+      },
+      error => {});
+  }
+
+  loadImageUrls() {
+    for (var i=0; i<this.items.length; i++) {
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.id)
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
         },
         error => {});
     }
+  }
 
-    loadImageUrls() {
-      for (var i=0; i<this.items.length; i++) {
-        let item = this.items[i];
-        this.produtoService.getSmallImageFromBucket(item.id)
-          .subscribe(response => {
-            item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
-          },
-          error => {});
-      }
-    }
+  showDetail(produto_id : string) {
+    this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
+  }
 
-    showDetail(produto_id : string) {
-      this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
-    }
-
- }
+}
