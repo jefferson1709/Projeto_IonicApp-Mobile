9688d6b0c49c431a7c26d3538f82c139dfa929da
Jefferson dos santos <Jefferson1709@hotmail.com>
2021-10-21 18:46:36 -0300

    Salvando novo cliente

--- SpringBootIonic/src/interceptors/error-interceptor.ts
+++ SpringBootIonic/src/interceptors/error-interceptor.ts
@@ -3,15 +3,16 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 import { StorageService } from '../services/storage.service';
 import { AlertController } from 'ionic-angular/components/alert/alert-controller';
+import { FieldMessage } from '../models/fieldmessage';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor(public storage: StorageService, public alertCtrl: AlertController){
-  }
+    constructor(public storage: StorageService, public alertCtrl: AlertController){
+    }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        //console.log("Passou no interceptor");
+      //console.log("Passou no interceptor");
         return next.handle(req)
         .catch((error, caught) => {
 
@@ -28,15 +29,19 @@ export class ErrorInterceptor implements HttpInterceptor {
 
             switch(errorObj.status) {
 
-              case 401:
+                case 401:
                 this.handle401();
                 break;
 
-              case 403:
-              this.handle403();
-              break;
+                case 403:
+                this.handle403();
+                break;
 
-              default:
+                case 422:
+                this.handle422(errorObj);
+                break;
+
+                default:
                 this.handleDefaultEror(errorObj);
             }
 
@@ -45,37 +50,58 @@ export class ErrorInterceptor implements HttpInterceptor {
     }
 
     handle403() {
-      this.storage.setLocalUser(null);
+        this.storage.setLocalUser(null);
     }
 
     handle401() {
-      let alert = this.alertCtrl.create({
-          title: 'Erro 401: falha de autenticação',
-          message: 'Email ou senha incorretos',
-          enableBackdropDismiss: false,
-          buttons: [
-              {
-                  text: 'Ok'
-              }
-          ]
-      });
-      alert.present();
-  }
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401: falha de autenticação',
+            message: 'Email ou senha incorretos',
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
 
-  handleDefaultEror(errorObj) {
-      let alert = this.alertCtrl.create({
-          title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
-          message: errorObj.message,
-          enableBackdropDismiss: false,
-          buttons: [
-              {
-                  text: 'Ok'
-              }
-          ]
-      });
-      alert.present();
-  }
+    handle422(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 422: Validação',
+            message: this.listErrors(errorObj.errors),
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
 
+    handleDefaultEror(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+        alert.present();
+    }
+
+    private listErrors(messages : FieldMessage[]) : string {
+        let s : string = '';
+        for (var i=0; i<messages.length; i++) {
+            s = s + '<p><strong>' + messages[i].fieldName + "</strong>: " + messages[i].message + '</p>';
+        }
+        return s;
+    }
 }
 
 export const ErrorInterceptorProvider = {
--- SpringBootIonic/src/pages/signup/signup.ts
+++ SpringBootIonic/src/pages/signup/signup.ts
@@ -5,6 +5,8 @@ import { CidadeService } from '../../services/domain/cidade.service';
 import { EstadoService } from '../../services/domain/estado.service';
 import { EstadoDTO } from '../../models/estado.dto';
 import { CidadeDTO } from '../../models/cidade.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 
 /**
  * Generated class for the SignupPage page.
@@ -25,11 +27,9 @@ export class SignupPage {
   cidades: CidadeDTO[];
 
   constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    public formBuilder: FormBuilder,
-    public cidadeService: CidadeService,
-    public estadoService: EstadoService) {
+    public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder,
+    public cidadeService: CidadeService, public estadoService: EstadoService, public clienteService: ClienteService,
+    public alertCtrl: AlertController) {
 
     this.formGroup = this.formBuilder.group({
       nome: ['teste', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
@@ -71,7 +71,28 @@ export class SignupPage {
   }
 
   signupUser() {
-    console.log("enviou o form");
+    this.clienteService.insert(this.formGroup.value)
+    .subscribe(response => {
+      this.showInsertOk();
+    },
+    error => {});
+  }
+
+  showInsertOk() {
+    let alert = this.alertCtrl.create({
+      title: 'Sucesso!',
+      message: 'Cadastro efetuado com sucesso',
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: 'Ok',
+          handler: () => {
+            this.navCtrl.pop();
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
 }
