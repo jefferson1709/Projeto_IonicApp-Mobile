dcbc1113817d962900e7a4b1fdc84d575cb86661
Jefferson dos santos <Jefferson1709@hotmail.com>
2021-10-22 10:41:50 -0300

    Componente loading e Refresher

--- SpringBootIonic/src/pages/produtos/produtos.ts
+++ SpringBootIonic/src/pages/produtos/produtos.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { API_CONFIG } from '../../config/api.config';
 import { ProdutoService } from '../../services/domain/produto.service';
+import { LoadingController } from 'ionic-angular/components/loading/loading-controller';
 
 /**
  * Generated class for the ProdutosPage page.
@@ -11,42 +12,66 @@ import { ProdutoService } from '../../services/domain/produto.service';
  * Ionic pages and navigation.
  */
  @IonicPage()
-@Component({
-  selector: 'page-produtos',
-  templateUrl: 'produtos.html',
-})
-export class ProdutosPage {
+ @Component({
+   selector: 'page-produtos',
+   templateUrl: 'produtos.html',
+ })
+ export class ProdutosPage {
 
-  items : ProdutoDTO[];
+   items : ProdutoDTO[];
 
-  constructor( public navCtrl: NavController, public navParams: NavParams,
-    public produtoService: ProdutoService) {
+   constructor(
+     public navCtrl: NavController,
+     public navParams: NavParams,
+     public produtoService: ProdutoService,
+     public loadingCtrl: LoadingController) {
+   }
 
-  }
+   ionViewDidLoad() {
+     this.loadData();
+   }
 
-  ionViewDidLoad() {
-    let categoria_id = this.navParams.get('categoria_id');
-    this.produtoService.findByCategoria(categoria_id)
-      .subscribe(response => {
-        this.items = response['content'];
-        this.loadImageUrls();
-      },
-      error => {});
-  }
+   loadData() {
+     let categoria_id = this.navParams.get('categoria_id');
+     let loader = this.presentLoading();
+     this.produtoService.findByCategoria(categoria_id)
+       .subscribe(response => {
+         this.items = response['content'];
+         loader.dismiss();
+         this.loadImageUrls();
+       },
+       error => {
+         loader.dismiss();
+       });
+   }
 
-  loadImageUrls() {
-    for (var i=0; i<this.items.length; i++) {
-      let item = this.items[i];
-      this.produtoService.getSmallImageFromBucket(item.id)
-        .subscribe(response => {
-          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
-        },
-        error => {});
-    }
-  }
+   loadImageUrls() {
+     for (var i=0; i<this.items.length; i++) {
+       let item = this.items[i];
+       this.produtoService.getSmallImageFromBucket(item.id)
+         .subscribe(response => {
+           item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+         },
+         error => {});
+     }
+   }
 
-  showDetail(produto_id : string) {
-    this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
-  }
+   showDetail(produto_id : string) {
+     this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
+   }
 
-}
+   presentLoading() {
+     let loader = this.loadingCtrl.create({
+       content: "Aguarde..."
+     });
+     loader.present();
+     return loader;
+   }
+
+   doRefresh(refresher) {
+     this.loadData();
+     setTimeout(() => {
+       refresher.complete();
+     }, 1000);
+   }
+ }
